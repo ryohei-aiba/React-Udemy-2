0bddf8eca1886d2d44fb4d7f21c77b631992c99d
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-02-15 13:34:18 +0900

    Learning re-rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,13 +12,17 @@ export default function App() {
     setOpen(!open);
   };
 
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,18 +1,28 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
-  const { open } = props;
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  const data = [...Array(2000).keys()];
+
+  //data.forEach(() => {
+  //console.log("...");
+  //});
+
+  //console.log(data);
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>非表示</button>
         </div>
       ) : null}
     </>
   );
-};
+});
